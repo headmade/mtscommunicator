06c3e3368724f646793f701296a77350eab2e9d0
lev <relevv@gmail.com>
2014-09-19 23:28:59 +0400

    Add example of i18n templates

--- bin/template_example.rb
+++ bin/template_example.rb
@@ -1,9 +1,11 @@
 #!/usr/bin/env ruby -Ilib -rmtscommunicator
 
-require 'json'
-
 MtsCommunicator::Service.login = ENV['MTSC_LOGIN']
 MtsCommunicator::Service.password = ENV['MTSC_PASSWORD']
+
+I18n.load_path = Dir[File.dirname(__FILE__)+'/*.yml']
+I18n.enforce_available_locales = false
+I18n.backend.load_translations
 
 class UserSms < MtsCommunicator::Base
   templates hello: 'Hello dear %name%, your account was activated on %today%',
@@ -12,4 +14,9 @@ end
 
 UserSms.hello('7917xxxyyyy', {name: 'Alex', today: Time.now})
 UserSms.state('7917yyyxxxx', {name: 'Ivan', state: :blocked, reason: 'spam detected'})
+
+I18n.locale = :en
+UserSms.hi('7917xxxyyyy', {name: 'user'})
+I18n.locale = :fr
+UserSms.hi('7917yyyxxxx', {name: 'user'})
 
