5de725a7ec3712ee8ccf7418193cf76c838c69be
lev <relevv@gmail.com>
2014-09-19 18:05:10 +0400

    Add a base class for custom template senders

---
+++ lib/mtscommunicator/base.rb
@@ -0,0 +1,34 @@
+module MtsCommunicator
+  class Base
+    class << self
+
+      def templates(t=nil)
+        @templates = t if t.is_a?(Hash)
+        raise 'no template set' unless @templates.is_a?(Hash)
+        @templates
+      end
+
+      private
+
+      def send_template(to_ids, template, vars={})
+        message = templates[template]
+        raise "unknown template (#{template.to_s})" unless message
+
+        vars.keys.each do |k|
+          message.gsub!("%#{k}%", vars[k].to_s)
+        end
+        MtsCommunicator::Service.send_messages(to_ids, message)
+      end
+
+      def method_missing(meth, *args, &block)
+        if templates[meth]
+          send_template(args.shift, meth, *args)
+        else
+          super
+        end
+      end
+
+    end
+  end
+end
+
